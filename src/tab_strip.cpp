4cf54490474db099c8d7f2e2e87ceced51f5cb44
Greg Gauthier <gmgauthier@protonmail.com>
2026-09-11 22:22:26 +0100

    A few pixels of space between Calendar and To Do tabs.

--- src/tab_strip.cpp
+++ src/tab_strip.cpp
@@ -10,7 +10,7 @@ TabStrip::TabStrip() : Gtk::Box(Gtk::ORIENTATION_VERTICAL, 4)
   set_margin_top(28);
   set_margin_start(0);
   set_margin_end(4);
-  set_spacing(2);
+  set_spacing(6);
   cal_.set_size_request(72, 48);
   todo_.set_size_request(72, 48);
   cal_.set_relief(Gtk::RELIEF_NONE);
